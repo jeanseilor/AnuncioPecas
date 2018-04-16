b64605af822851dbf603becd84d4154ccb145648
= <jean-moreira@live.com>
2018-04-16 00:14:32 -0300

    Projeto criado

---
+++ src/main/java/com/anuncios/pecas/AnuncioPecasApplication.java
@@ -0,0 +1,12 @@
+package com.anuncios.pecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnuncioPecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnuncioPecasApplication.class, args);
+	}
+}
