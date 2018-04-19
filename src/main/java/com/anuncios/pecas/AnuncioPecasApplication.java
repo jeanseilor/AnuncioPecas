b9d3073c0ac69cc84b00fea021c2a8073aed7f61
= <jean-moreira@live.com>
2018-04-18 21:58:20 -0300

    Estado e cidade

--- src/main/java/com/anuncios/pecas/AnuncioPecasApplication.java
+++ src/main/java/com/anuncios/pecas/AnuncioPecasApplication.java
@@ -51,10 +51,11 @@ public class AnuncioPecasApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+	
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
-		
 		
 		Cidade c1 = new Cidade(null, "Uberlândia",est1);
 		Cidade c2 = new Cidade(null, "São Paulo",est2);
@@ -64,8 +65,8 @@ public class AnuncioPecasApplication implements CommandLineRunner{
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		
-		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
-		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+	
 	}
 }
