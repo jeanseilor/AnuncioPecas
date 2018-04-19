65174a61e967aba2156dab12c73b0cb66ac22e6b
= <jean-moreira@live.com>
2018-04-18 23:08:57 -0300

    Endpoint clientes/{id} disponivel

--- src/main/java/com/anuncios/pecas/resources/ClienteResources.java
+++ src/main/java/com/anuncios/pecas/resources/ClienteResources.java
@@ -8,20 +8,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.anuncios.pecas.domain.Categoria;
-import com.anuncios.pecas.services.CategoriaService;
+import com.anuncios.pecas.domain.Cliente;
+import com.anuncios.pecas.services.ClienteService;
 
 @RestController 
-@RequestMapping(value="/categorias")
-public class CategoriaResources {
+@RequestMapping(value="/clientes")
+public class ClienteResources {
 	
 	@Autowired
-	private CategoriaService service;
+	private ClienteService service;
 	
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
+		Cliente obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
---
+++ src/main/java/com/anuncios/pecas/services/ClienteService.java
@@ -0,0 +1,31 @@
+package com.anuncios.pecas.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.anuncios.pecas.domain.Cliente;
+import com.anuncios.pecas.repository.ClienteRepository;
+import com.anuncios.pecas.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	
+	/**
+	 * Busca uma categoria a parti do seu ID
+	 * @param id
+	 * @return retorna o cliente informada a parti do ID
+	 */
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+
+	}
+	
+}
