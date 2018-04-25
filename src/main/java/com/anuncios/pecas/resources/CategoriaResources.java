0d7b1ed62e34f9ffbbb478ad525cdd066526ef50
= <jean-moreira@live.com>
2018-04-24 21:20:57 -0300

    Deletando uma categoria com Delete

--- src/main/java/com/anuncios/pecas/resources/CategoriaResources.java
+++ src/main/java/com/anuncios/pecas/resources/CategoriaResources.java
@@ -44,5 +44,12 @@ public class CategoriaResources {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
 	
 }
--- src/main/java/com/anuncios/pecas/services/CategoriaService.java
+++ src/main/java/com/anuncios/pecas/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.anuncios.pecas.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.anuncios.pecas.domain.Categoria;
 import com.anuncios.pecas.repository.CategoriaRepository;
+import com.anuncios.pecas.services.exceptions.DataIntegrityException;
 import com.anuncios.pecas.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -36,5 +38,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Nao e possivel esxcluir uma categoria que possui produtos");
+		}
+		
 	}
 }
