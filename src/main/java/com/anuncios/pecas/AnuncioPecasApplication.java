0fcd0e4b7f341718fec4e03c6fd8b953022de57f
jeanMoreira <jean-moreira@live.com>
2018-04-26 11:33:25 -0300

    paginacao com parametros opcionais na requisicao

--- src/main/java/com/anuncios/pecas/AnuncioPecasApplication.java
+++ src/main/java/com/anuncios/pecas/AnuncioPecasApplication.java
@@ -69,6 +69,11 @@ public class AnuncioPecasApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "Eletronicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -81,7 +86,7 @@ public class AnuncioPecasApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	
 		Estado est1 = new Estado(null, "Minas Gerais");
